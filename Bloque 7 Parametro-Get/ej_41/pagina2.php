9f2de0c01b48571dcf96acc9fc42116e58d1cb56
Yuki <119506189+YukiiCode@users.noreply.github.com>
2024-11-22 09:24:58 +0100

    Delete Bloque 7 Parametro-Get/ej_41 directory

--- Bloque 7 Parametro-Get/ej_41/pagina2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ($_GET) {
-    echo "Datos del seleccionado<br>";
-    echo "Nombre: " . $_GET['Nombre'] . " " . $_GET['Apellidos'] . "<br>";
-    echo "Edad: " . $_GET['Edad'];
-}
-
-?>

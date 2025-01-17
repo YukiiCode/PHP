275cb35c7ae2328b283f5fd19c90ac10cfe55044
Yuki <119506189+YukiiCode@users.noreply.github.com>
2025-01-17 12:59:32 +0100

    Mejorar la visualización de tareas: actualizar enlaces, simplificar botones y corregir la lógica de despliegue de información extra

--- Proyecto-2/resources/views/ver_tareas.blade.php
+++ Proyecto-2/resources/views/ver_tareas.blade.php
@@ -28,30 +28,32 @@
                 <td>{{ $tarea->anotaciones }}</td>
                 <td>{{ $clientes[$tarea->cliente_id]->nombre }}</td>
                 <td>
-                    <a href="" class="btn btn-primary btn-sm">Detalles/Editar</a>
+                    <a href="" class="btn btn-primary btn-sm">Editar</a>
                     <a href="{{ route('borrar-tarea', ['id' => $tarea->id]) }}" class="btn btn-danger btn-sm">Eliminar</a>
                     <!-- Desplegable con mas datos -->
                     <div class="btn-group">
                         <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <a href="{{route('ver-tareas', ['id' => $tarea->id]) }}">  @if (isset($_GET['id']) && $tarea->id == $_GET['id'])Menos @else Mas @endif </a>
+                            <a style="text-decoration: none; color:white" href="{{ route('ver-tareas', ['id' => $tarea->id, 'page' => request()->query('page')]) }}"> @if (request()->query('id') == $tarea->id) Menos @else Mas @endif </a>
                         </button>
-                        <tr @if (isset($_GET['id']) && $tarea->id == $_GET['id']) style="display: block;" @else
-                            style="display: none;" @endif>
-                            <td>INFORMACION EXTRA</td>
-                        </tr>
-                    </div>
-                </td>
+            <tr
+                @if (isset($_GET['id']) && $tarea->id == $_GET['id'] && style="display: block; ) style="display: none;"
+                @elseif (isset($_GET['id']) && $tarea->id == $_GET['id']) style="display: block;" 
+                @else style="display: none;" @endif>
+                <td>INFORMACION EXTRA</td>
             </tr>
-            @endforeach
-        </tbody>
-    </table>
+</div>
+</td>
+</tr>
+@endforeach
+</tbody>
+</table>
 
-    <!-- Paginacion - 10 tareas por pagina -->
+<!-- Paginacion - 10 tareas por pagina -->
 
-    <div class="d-flex justify-content-center">
-        {{ $tareas->links('vendor.pagination.default') }}
-    </div>
+<div class="d-flex justify-content-center">
+    {{ $tareas->links('vendor.pagination.default') }}
+</div>
 
 </div>
 @endsection

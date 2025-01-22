9330bbf45729986480769f758f2fac2e138e038b
Yuki <119506189+YukiiCode@users.noreply.github.com>
2025-01-22 09:30:39 +0100

    Actualizar valores del enum en la tabla tarea para incluir 'T' como estado adicional

--- Proyecto-2/sqlNUEVO.sql
+++ Proyecto-2/sqlNUEVO.sql
@@ -41,7 +41,7 @@ CREATE TABLE
 CREATE TABLE
     tarea (
         id INT PRIMARY KEY AUTO_INCREMENT,
-        estado ENUM ('F','C','C','A','E'), -- Add the possible values for the enum
+        estado ENUM ('F','T','C','A','E'), -- Add the possible values for the enum
         -- Finalizada , En curso, Cancelada, Esperando ser aprobada, Esperando ser asignada por un administrador
         operario_encargado INT,
         fecha_creacion DATE,
